714ddd3e4e84ea7e553c104d694633419b5838eb
anusha-mithun <anusha-mithun@gmail.com>
2023-09-09 18:58:27 +0000

    aboutme

---
+++ aboutme.sh
@@ -0,0 +1,13 @@
+hello
+how are you all in india
+i am missing you all very badly
+remembering evreryone very badly
+i really really wanted to come back to you
+i am feeling very sad 
+there is no one to know about me at this moment, thinking of you everyone 
+thanks for everything for bearing me about 24 years 
+i am sorry for leaving you all and making you sad 
+i chose a path called "selfish", so everything is repaying back now. 
+thank you guys for everything, i really really want to come back to india , i dont know why but feeling very dippressed, lonely, thinking of doing something for myself.
+ I dont even know why, but i am not liking the way i am living at this momemt. 
+
