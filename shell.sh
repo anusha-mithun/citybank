4de70893d7b216c5e812abd053a967488c1ea282
anusha-mithun <anusha-mithun@gmail.com>
2023-09-14 01:54:33 +0000

    first script

---
+++ shell.sh
@@ -0,0 +1,13 @@
+a=10
+b=20
+c=30
+name=anusha
+
+echo "a varaible value is: " $a
+echo "b variable value is: " $b
+echo "c variable value is: " $c
+echo "anusha variable value is: " $name
+echo $a
+echo $b
+echo $c
+echo $name
